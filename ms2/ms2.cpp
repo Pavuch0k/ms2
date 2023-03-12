147deab200481ce1faa5f269dc7bcb92ba39def5
Никита Губанов <gubanovnikita783@gmail.com>
2023-03-12 16:15:04 +0400

    Добавьте файлы проекта.

---
+++ ms2/ms2.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	setlocale(LC_ALL, "RUSSIAN");
+	int a[100];
+	int n;
+	cout << "Введите количество элементов массива : ";
+	cin >> n;
+	cout << "Заполните массив" << endl;
+	for (int i = 0; i < n; i++)
+	{
+		cout << "Введите " << i + 1 << " элемент массива: ";
+		cin >> a[i];
+	}
+	int sum=0;
+	for (int i = 0; i < n; i++)
+	{
+		sum =sum+a[i];
+	}
+	cout << "Среднее арифметическое занчение элементов массива: " << sum;
+
+	int q=0;
+	for (int i = 0; i < n; i++)
+	{
+		if (a[i] < 0)
+			q = q + 1;
+		
+	}
+	cout <<endl <<"Количество отрицательных элементов: " << q;
+}
+
+
